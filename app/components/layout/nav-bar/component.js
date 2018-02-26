bfd0829931559765bc0a141d1792ae67cfa5a638
Jesus Martinez <jesusda91@gmail.com>
2018-02-26 11:38:15 -0500

    initial layout

---
+++ app/components/layout/nav-bar/component.js
@@ -0,0 +1,7 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+	classNames: ["nav-bar", "mdl-layout__header", "mdl-color--grey-100", "mdl-color-text--grey-600"],
+	tagName: "header"
+
+});
---
+++ app/components/layout/side-bar/component.js
@@ -0,0 +1,5 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+	classNames: ["side-bar", "layout-drawer", "mdl-layout__drawer", "mdl-color--blue-grey-900", "mdl-color-text--blue-grey-50"],
+});
