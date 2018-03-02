493f7726b34d0413088ea75882eb0f9074d676a7
Jesus Martinez <jesusda91@gmail.com>
2018-03-02 15:40:16 -0500

    add i18n initializer

--- app/initializers/i18n.js
+++ app/initializers/i18n.js
@@ -1,7 +1,11 @@
-export function initialize(/* application */) {
-  // application.inject('route', 'foo', 'service:foo');
+export function initialize(application) {
+	application.inject('route', 'i18n', 'service:i18n');
+	application.inject('model', 'i18n', 'service:i18n');
+	application.inject('controller', 'i18n', 'service:i18n');
+	application.inject('component', 'i18n', 'service:i18n');
 }
 
 export default {
-  initialize
+	name: 'i18n',
+	initialize
 };
